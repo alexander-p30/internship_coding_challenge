71ffb49777e5ccaa8a47bd75f6ccc6311ace5192
alexander-p30 <alexanderp30e@gmail.com>
2020-10-13 11:28:58 -0300

    :sparkles: Adds a seeder to populate the app's classes
    This seeder takes the api's responses and uses the provided data to populate de application's classes

---
+++ utils/seeder.rb
@@ -0,0 +1,31 @@
+require_relative '../cm_challenge/api'
+require_relative '../models/absence'
+require_relative '../models/member'
+
+KLASSES = {
+  %i[name user_id crew_id] => Models::Member,
+  %i[user_id type start_date end_date] => Models::Absence
+}.freeze
+
+# Seeder class used to populate the app's classes with the api responses
+class Seeder
+  class << self
+    def populate_classes_from_api
+      api = CmChallenge::Api
+      (api.members + api.absences).each do |raw_object_data|
+        create_object(raw_object_data)
+      end
+    end
+
+    private
+
+    def create_object(data_hash)
+      KLASSES.each do |necessary_data, klass|
+        if necessary_data.all? { |data_key| data_hash.key? data_key }
+          return klass.new(data_hash)
+        end
+      end
+      raise ArgumentError, 'Received hash does not match any class model.'
+    end
+  end
+end
