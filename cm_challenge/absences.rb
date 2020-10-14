fea60ec8d347be8533934740719f97a4275f2bc9
alexander-p30 <alexanderp30e@gmail.com>
2020-10-14 18:11:57 -0300

    :art: Separated date managing to a different class
    So CmChallenge::Absences takes care of only manipulating the data, but using auxiliary methods from this class when it comes to managing dates

--- cm_challenge/absences.rb
+++ cm_challenge/absences.rb
@@ -1,6 +1,7 @@
 require_relative './api'
 require_relative '../models/absence'
 require_relative '../models/member'
+require_relative '../utils/date_helper'
 require 'icalendar'
 require 'date'
 
@@ -16,18 +17,22 @@ module CmChallenge
       end
 
       def list(user, start_date, end_date)
-        confirmed = Models::Absence.confirmed_absences
-        pending = Models::Absence.pending_absences
-
-        start_date ||= '1900-01-01'
-        end_date ||= '9999-12-31'
-
         if user
           confirmed = user.confirmed_absences
           pending = user.pending_absences
+        else
+          confirmed = Models::Absence.confirmed_absences
+          pending = Models::Absence.pending_absences
         end
 
-        filter_absences_by_date(confirmed, pending, start_date, end_date)
+        start_date ||= DateHelper::DEFAULT_START_DATE
+        end_date ||= DateHelper::DEFAULT_END_DATE
+
+        DateHelper.absences_by_date(confirmed, pending, start_date, end_date)
+      end
+
+      def find_member_by_user_id(user_id)
+        Models::Member.find_by_user_id(user_id)
       end
 
       private
@@ -38,37 +43,6 @@ module CmChallenge
         e.dtend = Icalendar::Values::Date.new absence.end_date.tr('-', '')
         e.summary = absence.description
         e
-      end
-
-      def create_absence_hash(absence)
-        { absentee_name: absence.absentee.name,
-          start_date: absence.start_date,
-          end_date: absence.end_date,
-          description: absence.description }
-      end
-
-      def filter_absences_by_date(confirmed, pending, start_date, end_date)
-        start_date = Date.parse(start_date)
-        end_date = Date.parse(end_date)
-
-        [confirmed.select do |abs|
-           check_date_range(abs, start_date, end_date)
-         end,
-         pending.select do |abs|
-           check_date_range(abs, start_date, end_date)
-         end]
-      end
-
-      def check_date_range(absence, start_date, end_date)
-        abs_start_date = absence.start_date_as_date
-        abs_end_date = absence.end_date_as_date
-
-        starts_within_time = (abs_start_date >= start_date &&
-                                abs_start_date <= end_date)
-        ends_within_time = (abs_end_date >= start_date &&
-                              abs_end_date <= end_date)
-
-        starts_within_time || ends_within_time
       end
     end
   end
---
+++ utils/date_helper.rb
@@ -0,0 +1,30 @@
+# Module that holds methods for date managing
+module DateHelper
+  DEFAULT_START_DATE = '1900-01-01'.freeze
+  DEFAULT_END_DATE = '9999-12-31'.freeze
+
+  def self.absences_by_date(confirmed, pending, start_date, end_date)
+    start_date, end_date = parse_params_as_date(start_date, end_date)
+
+    [confirmed.select { |abs| check_date_range(abs, start_date, end_date) },
+     pending.select { |abs| check_date_range(abs, start_date, end_date) }]
+  end
+
+  def self.parse_params_as_date(start_date, end_date)
+    [Date.parse(start_date), Date.parse(end_date)]
+  rescue ArgumentError
+    [Date.parse(DEFAULT_START_DATE), Date.parse(DEFAULT_END_DATE)]
+  end
+
+  def self.check_date_range(absence, start_date, end_date)
+    abs_start_date = absence.start_date_as_date
+    abs_end_date = absence.end_date_as_date
+
+    starts_within_time = (abs_start_date >= start_date &&
+        abs_start_date <= end_date)
+    ends_within_time = (abs_end_date >= start_date &&
+        abs_end_date <= end_date)
+
+    starts_within_time || ends_within_time
+  end
+end
