5f9d2ab1c640c5dcb0ea625fe7ea4cde1ab5b566
alexander-p30 <alexanderp30e@gmail.com>
2020-10-14 15:02:42 -0300

    :white_check_mark: Corrects tests for CmChallenge::Absences
    Tests were expecting the old date comparison model (absences that start AND end within the time frame)

--- spec/absences_spec.rb
+++ spec/absences_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Absences' do
 
   before(:example) do
     @user = Models::Member.find_by_user_id(644)
-    @start_date = nil
+    @start_date = '2017-01-01'
     @end_date = '2017-03-13'
   end
 
@@ -34,21 +34,25 @@ RSpec.describe 'Absences' do
       let(:list) { @absences_controller.list(@user, @start_date, @end_date) }
       let(:users_confirmed_absences) do
         @user.confirmed_absences.select do |abs|
-          Date.parse(abs.start_date) >= Date.parse(@start_date) &&
-            Date.parse(abs.end_date) <= Date.parse(@end_date)
+          (abs.start_date_as_date >= Date.parse(@start_date) ||
+            abs.start_date_as_date <= Date.parse(@end_date)) ||
+            (abs.end_date_as_date >= Date.parse(@start_date) ||
+              abs.end_date_as_date <= Date.parse(@end_date))
         end
       end
       let(:users_pending_absences) do
-        @user.pending_absenses.select do |abs|
-          Date.parse(abs.start_date) >= Date.parse(@start_date) &&
-            Date.parse(abs.end_date) <= Date.parse(@end_date)
+        @user.pending_absences.select do |abs|
+          (abs.start_date_as_date >= Date.parse(@start_date) ||
+            abs.start_date_as_date <= Date.parse(@end_date)) ||
+            (abs.end_date_as_date >= Date.parse(@start_date) ||
+              abs.end_date_as_date <= Date.parse(@end_date))
         end
       end
 
       # Checks if all the confirmed/pending absences for the time period are
       # correctly returned
-      it { expect(list[0].length).to eql(6) }
-      it { expect(list[1].length).to eql(1) }
+      it { expect(list[0].length).to eql(users_confirmed_absences.length) }
+      it { expect(list[1].length).to eql(users_pending_absences.length) }
       # Checks if returned list contains only objects of the Absence
       it { expect(list.flatten).to all(respond_to(:user_id)) }
       it { expect(list.flatten).to all(respond_to(:type)) }
